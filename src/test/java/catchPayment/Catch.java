5d286ed40e607243b7a5fe7966555796a7d34e40
Harini Venkateshan <harini.venkateshan@accesshq.com>
2017-07-07 12:35:33 +1000

    New Catch created Signed-off-by: Harini Venkateshan <harini.venkateshan@accesshq.com>

--- src/test/java/catchPayment/Catch.java
+++ src/test/java/catchPayment/Catch.java
@@ -17,7 +17,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
+//Testing git.
 
 public class Catch {
 
